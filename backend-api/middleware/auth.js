c79e5ea54bc404952be1bba009ec83a2776494eb
nghĩa trần <Nghiateprieunew@gmail.com>
2026-07-15 03:20:48 +0700

    thêm trang quản lý tài khoản

--- backend-api/middleware/auth.js
+++ backend-api/middleware/auth.js
@@ -1,17 +1,16 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'chuoibimatcuanghia123!@#'; // Chìa khóa tạo vé thông hành
 
 const verifyToken = (req, res, next) => {
     const token = req.header('auth-token');
-    if (!token) return res.status(401).json({ message: 'Truy cập bị từ chối! Vui lòng đăng nhập.' });
+    if (!token) return res.status(401).json({ message: 'Không tìm thấy vé thông hành (Access Denied)' });
 
     try {
-        const verified = jwt.verify(token, JWT_SECRET);
-        req.user = verified; // Trả về thông tin của user
-        next(); // Cho đi tiếp
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET || 'BiMatCuaTao');
+        req.user = verified;
+        next(); 
     } catch (err) {
-        res.status(400).json({ message: 'Token không hợp lệ hoặc đã hết hạn!' });
+        res.status(400).json({ message: 'Vé thông hành không hợp lệ (Invalid Token)' });
     }
 };
 
-module.exports = { verifyToken, JWT_SECRET };
+module.exports = { verifyToken };
